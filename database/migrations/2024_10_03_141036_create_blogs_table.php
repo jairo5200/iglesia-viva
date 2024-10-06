930f4a731497dba5a42ed06a5a62c094777e523f
jairo5200 <jairo5200@gmail.com>
2024-10-06 18:57:53 -0500

    agregamos documentacion faltante

--- database/migrations/2024_10_03_141036_create_blogs_table.php
+++ database/migrations/2024_10_03_141036_create_blogs_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * se crea la tabla blogs en la base de datos
      */
     public function up(): void
     {
